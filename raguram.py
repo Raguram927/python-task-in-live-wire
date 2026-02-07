a1215980812feb6a50a05aa8465d1082722f570b
Raguram927 <raguram9271@gmail.com>
2026-02-07 13:09:15 +0530

    Add files via upload

---
+++ raguram.py
@@ -0,0 +1,45 @@
+'''i=int(input("enter a natural number"))
+for i in range (0,10,1):
+    print ( i+1)'''
+'''i=int(input("enter a number"))
+for i in range (1,i+1,2):
+    print (i+1)'''
+'''i=int(input("enter a number"))
+for i in range (1,10,2):
+    print (i)'''
+'''n=3
+num=int(input("enter a number"))
+for num in range (1,11,1):
+    print(num,'X3',3*num)'''
+
+
+'''s=int(input("enter a number"))
+for s in range (1,11,1):
+    print(s,'X5',5*s)'''
+
+'''s=int(input("enter a number"))
+for s in range (1,11,1):
+    print(2*s)'''
+'''n=10
+s=int(input("enter a number"))
+for i in range (1,n+1,1):
+
+   if i%2==0 or i%3==0:
+    print (i)'''''
+'''n=10
+s=int(input("enter a number"))
+for i in range (1,n+1,1):
+
+   if i%2==0 or i%5==0 or i%7==0:
+    print (i)'''
+'''n=10
+s=int(input("enter a number"))
+for i in range (1,n+1,1):
+
+   if i%3==0 or i%5==0 or i%7==0:
+    print (i)
+'''
+
+
+s=int(input("enter a number"))
+for i in range (1,11,1):
