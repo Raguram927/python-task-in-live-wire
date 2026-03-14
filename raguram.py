41dcce8af1c53e38c494a539bbe466ef9932de6e
Raguram927 <raguram9271@gmail.com>
2026-03-14 13:11:17 +0530

    Add files via upload

--- raguram.py
+++ raguram.py
@@ -1,45 +1,91 @@
-'''i=int(input("enter a natural number"))
-for i in range (0,10,1):
-    print ( i+1)'''
-'''i=int(input("enter a number"))
-for i in range (1,i+1,2):
-    print (i+1)'''
-'''i=int(input("enter a number"))
-for i in range (1,10,2):
-    print (i)'''
-'''n=3
-num=int(input("enter a number"))
-for num in range (1,11,1):
-    print(num,'X3',3*num)'''
-
-
-'''s=int(input("enter a number"))
-for s in range (1,11,1):
-    print(s,'X5',5*s)'''
-
-'''s=int(input("enter a number"))
-for s in range (1,11,1):
-    print(2*s)'''
-'''n=10
-s=int(input("enter a number"))
-for i in range (1,n+1,1):
-
-   if i%2==0 or i%3==0:
-    print (i)'''''
-'''n=10
-s=int(input("enter a number"))
-for i in range (1,n+1,1):
-
-   if i%2==0 or i%5==0 or i%7==0:
-    print (i)'''
-'''n=10
-s=int(input("enter a number"))
-for i in range (1,n+1,1):
-
-   if i%3==0 or i%5==0 or i%7==0:
-    print (i)
-'''
-
-
-s=int(input("enter a number"))
-for i in range (1,11,1):
+'''a={'id1':"raguram",'id2':"hari",'id3':"lokesh"}
+print(a)
+print(a["id1"])
+print(a.keys())
+print(a.get("id2"))
+a.pop("id1")
+print(a)
+for i in a:
+    print(i,":",a[a])
+    a.update({"id4":"raguram"})
+    print (a)
+a.clear()
+print(a)
+del a
+print(a)
+def demo():
+    print("HI")
+demo()
+def add():
+    a=10
+    b=10
+print(a+b)
+add()'''
+#list
+'''a=[1,2,3,4,5,6]
+print(a[0])
+print(a[:])
+print(a[:5])
+print(a[::-1])
+a=[1,2,3,4,5,6]
+b=[2,4,6,8,10]
+c=a+b
+print(a*2)'''
+'''#adding elements in the list:
+#appent
+a=[1,2,3,4,5]
+a.append(6)
+print(a)
+a.extend([7,8])
+print(a)
+a.insert(1,2)
+print(a)
+#removing
+a.remove(8)
+print(a)
+a.pop()
+print(a)
+a.pop(5)
+print(a)
+#duplicate removing
+x=[1,2,3,5,7,8,9,56,43]
+y=[1,2,3,4,5]
+y=[]
+for i in x:
+    if i not in y:
+        y.append [1]
+    print y'''
+'''#modification
+z=[1,2,3,4,5]
+   z=3
+print(z)'''
+'''#functions
+i=[1,2,3,4,5,6,7,8,9]
+print(max(1))
+print(min(1))
+print[i.count(5)]
+i.sort()
+print(i.index(5))
+i.reverse()'''
+'''#tuple
+t1=[1,2,3,4,5]
+t2=["apple","banana","cherry"]
+t3=[i,hello,3,4,true]
+t4=[6,7,8]
+print("slice:"[14])
+print "concet:"(at:t1+ta)
+print ("repeat:"t2*2)
+print(3 in t1:", 3 in t1)
+print "tuple from list:":tuble([1,2,3])
+print ("tuple from string :, tuple("python"))
+return (a+b,a-b,a*b,a/b)
+print("starts tuple:',start (0,3))
+x=(1,2,4,5,6,7,8,9)
+x=list(x)
+x=()
+s={1,2,3,4,5,6,7,8,9}
+print(s)
+s.add(4)
+print(5)
+s=update(1,2,3,4,5)
+print (s)'''
